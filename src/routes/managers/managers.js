046eff0e08b4db53b042937ba832d376877344cb
noahjt2 <88336605+noahjt2@users.noreply.github.com>
2021-08-02 11:48:42 -0400

    Update managers.js

--- src/routes/managers/managers.js
+++ src/routes/managers/managers.js
@@ -1,14 +1,182 @@
 export const managers = [
-  // {
-  //   "roster": 1,  // ID of the roster that the manager manages
-  //   "name": "Your Name",
+   {
+     "roster": 1,  // ID of the roster that the manager manages
+     "name": "Noah Thompson",
   //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
   //   "location": "Brooklyn",
-  //   "bio": "Lorem ipsum...",
+     "bio": "In Trev we Trust",
   //   "photo": "/managers/name.jpg", // done
   //   "fantasyStart": 2014, // when did the manager start playing fantasy football
   //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-  //   "mode": "Win Now", // 'Win Now' or 'Rebuild'
+     "mode": "All I do is Win", // 'Win Now' or 'Rebuild'
+  //   "rival": {
+  //     name: "Rival", // Can be anything (usually your rival's name)
+  //     link: 6, // manager array number within this array, or null to link back to all managers page
+  //     image: "/managers/rival.jpg", // either a specific manager photo or 'everyone' or 'question'
+  //   },
+  //   "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+  //   "valuePosition": "WR", // Favorite position (QB, WR, RB, TE, etc.)
+  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets'
+  //   "philosophy": "Your fantasy team's philosophy",
+  //   "tradingScale": 10, // 1 - 10
+  //   "prefferedContact": "Text", // 'Text', 'WhatsApp', and 'Carrier Pigeon' are currently supplied in the template
+  // },
+]
+     export const managers = [
+   {
+     "roster": 2,  // ID of the roster that the manager manages
+     "name": "Gaston Sanders",
+  //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+  //   "location": "Brooklyn",
+  //   "bio": "",
+  //   "photo": "/managers/name.jpg", // done
+  //   "fantasyStart": 2014, // when did the manager start playing fantasy football
+  //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+  //   "mode": "Rebuild", // 'Win Now' or 'Rebuild'
+  //   "rival": {
+  //     name: "Rival", // Can be anything (usually your rival's name)
+  //     link: 6, // manager array number within this array, or null to link back to all managers page
+  //     image: "/managers/rival.jpg", // either a specific manager photo or 'everyone' or 'question'
+  //   },
+  //   "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+  //   "valuePosition": "WR", // Favorite position (QB, WR, RB, TE, etc.)
+  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets'
+  //   "philosophy": "Your fantasy team's philosophy",
+  //   "tradingScale": 10, // 1 - 10
+  //   "prefferedContact": "Text", // 'Text', 'WhatsApp', and 'Carrier Pigeon' are currently supplied in the template
+  // },
+]
+export const managers = [
+   {
+     "roster": 3,  // ID of the roster that the manager manages
+     "name": "Josh Parrish",
+  //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+  //   "location": "Brooklyn",
+  //   "bio": "In Trev we Trust",
+  //   "photo": "/managers/name.jpg", // done
+  //   "fantasyStart": 2014, // when did the manager start playing fantasy football
+  //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+  //   "mode": "All I do is Win", // 'Win Now' or 'Rebuild'
+  //   "rival": {
+  //     name: "Rival", // Can be anything (usually your rival's name)
+  //     link: 6, // manager array number within this array, or null to link back to all managers page
+  //     image: "/managers/rival.jpg", // either a specific manager photo or 'everyone' or 'question'
+  //   },
+  //   "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+  //   "valuePosition": "WR", // Favorite position (QB, WR, RB, TE, etc.)
+  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets'
+  //   "philosophy": "Your fantasy team's philosophy",
+  //   "tradingScale": 10, // 1 - 10
+  //   "prefferedContact": "Text", // 'Text', 'WhatsApp', and 'Carrier Pigeon' are currently supplied in the template
+  // },
+]
+     export const managers = [
+   {
+     "roster": 4,  // ID of the roster that the manager manages
+     "name": "Evan Davis",
+  //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+  //   "location": "Brooklyn",
+  //   "bio": "In Trev we Trust",
+  //   "photo": "/managers/name.jpg", // done
+  //   "fantasyStart": 2014, // when did the manager start playing fantasy football
+  //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+  //   "mode": "All I do is Win", // 'Win Now' or 'Rebuild'
+  //   "rival": {
+  //     name: "Rival", // Can be anything (usually your rival's name)
+  //     link: 6, // manager array number within this array, or null to link back to all managers page
+  //     image: "/managers/rival.jpg", // either a specific manager photo or 'everyone' or 'question'
+  //   },
+  //   "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+  //   "valuePosition": "WR", // Favorite position (QB, WR, RB, TE, etc.)
+  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets'
+  //   "philosophy": "Your fantasy team's philosophy",
+  //   "tradingScale": 10, // 1 - 10
+  //   "prefferedContact": "Text", // 'Text', 'WhatsApp', and 'Carrier Pigeon' are currently supplied in the template
+  // },
+]
+export const managers = [
+   {
+     "roster": 5,  // ID of the roster that the manager manages
+     "name": "Ryan Davis",
+  //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+  //   "location": "Brooklyn",
+  //   "bio": "In Trev we Trust",
+  //   "photo": "/managers/name.jpg", // done
+  //   "fantasyStart": 2014, // when did the manager start playing fantasy football
+  //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+  //   "mode": "All I do is Win", // 'Win Now' or 'Rebuild'
+  //   "rival": {
+  //     name: "Rival", // Can be anything (usually your rival's name)
+  //     link: 6, // manager array number within this array, or null to link back to all managers page
+  //     image: "/managers/rival.jpg", // either a specific manager photo or 'everyone' or 'question'
+  //   },
+  //   "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+  //   "valuePosition": "WR", // Favorite position (QB, WR, RB, TE, etc.)
+  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets'
+  //   "philosophy": "Your fantasy team's philosophy",
+  //   "tradingScale": 10, // 1 - 10
+  //   "prefferedContact": "Text", // 'Text', 'WhatsApp', and 'Carrier Pigeon' are currently supplied in the template
+  // },
+]
+     export const managers = [
+   {
+     "roster": 6,  // ID of the roster that the manager manages
+     "name": "Josh Apple",
+  //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+  //   "location": "Brooklyn",
+  //   "bio": "In Trev we Trust",
+  //   "photo": "/managers/name.jpg", // done
+  //   "fantasyStart": 2014, // when did the manager start playing fantasy football
+  //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+  //   "mode": "All I do is Win", // 'Win Now' or 'Rebuild'
+  //   "rival": {
+  //     name: "Rival", // Can be anything (usually your rival's name)
+  //     link: 6, // manager array number within this array, or null to link back to all managers page
+  //     image: "/managers/rival.jpg", // either a specific manager photo or 'everyone' or 'question'
+  //   },
+  //   "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+  //   "valuePosition": "WR", // Favorite position (QB, WR, RB, TE, etc.)
+  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets'
+  //   "philosophy": "Your fantasy team's philosophy",
+  //   "tradingScale": 10, // 1 - 10
+  //   "prefferedContact": "Text", // 'Text', 'WhatsApp', and 'Carrier Pigeon' are currently supplied in the template
+  // },
+]
+export const managers = [
+   {
+     "roster": 7,  // ID of the roster that the manager manages
+     "name": "Zane Kaiser",
+  //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+  //   "location": "Brooklyn",
+  //   "bio": "In Trev we Trust",
+  //   "photo": "/managers/name.jpg", // done
+  //   "fantasyStart": 2014, // when did the manager start playing fantasy football
+  //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+  //   "mode": "All I do is Win", // 'Win Now' or 'Rebuild'
+  //   "rival": {
+  //     name: "Rival", // Can be anything (usually your rival's name)
+  //     link: 6, // manager array number within this array, or null to link back to all managers page
+  //     image: "/managers/rival.jpg", // either a specific manager photo or 'everyone' or 'question'
+  //   },
+  //   "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+  //   "valuePosition": "WR", // Favorite position (QB, WR, RB, TE, etc.)
+  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets'
+  //   "philosophy": "Your fantasy team's philosophy",
+  //   "tradingScale": 10, // 1 - 10
+  //   "prefferedContact": "Text", // 'Text', 'WhatsApp', and 'Carrier Pigeon' are currently supplied in the template
+  // },
+]
+     export const managers = [
+   {
+     "roster": 8,  // ID of the roster that the manager manages
+     "name": "Alex Bonar",
+  //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+  //   "location": "Brooklyn",
+  //   "bio": "In Trev we Trust",
+  //   "photo": "/managers/name.jpg", // done
+  //   "fantasyStart": 2014, // when did the manager start playing fantasy football
+  //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+  //   "mode": "All I do is Win", // 'Win Now' or 'Rebuild'
   //   "rival": {
   //     name: "Rival", // Can be anything (usually your rival's name)
   //     link: 6, // manager array number within this array, or null to link back to all managers page
